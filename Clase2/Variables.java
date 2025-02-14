1f5bbd08c2560299ed5c93dd6db4e883c2bba8a6
SBarona07 <juan.barona@campusucc.edu.co>
2025-02-14 01:22:38 +0000

    Ejercicios Clase2

---
+++ Clase2/Variables.java
@@ -0,0 +1,17 @@
+public class Variables {
+    // Metodo main
+    public static void main(String[] args) {
+
+        // Tipos de datos+
+        // Enteros int, long
+        int edad = 25;
+        // Reales double, float
+        double altura = 1.75;
+        // Cadenas
+        String nombre = "Juan";
+        // Logicos
+        boolean esEstudiante = true; // false
+        System.out.println("Nombre: " + nombre + ", Edad: " + edad + ", Altura: " + altura + ", Estudiante: " + esEstudiante + "");
+
+    }
+}
