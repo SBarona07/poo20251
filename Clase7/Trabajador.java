9b77c0ecd09e448621b4015a282a70d95b8938cd
SBarona07 <juan.barona@campusucc.edu.co>
2025-03-21 00:02:49 +0000

    Creacion de la clase Trabajadora

---
+++ Clase7/Trabajador.java
@@ -0,0 +1,37 @@
+package Clase7;
+
+public class Trabajador {
+    
+    //Atributos
+    private int idTrabajador;
+    private String nombre;
+    private String apellido;
+
+    //Constructor
+    public Trabajador(int idTrabajador, String Trabajador, String nombre, String apellido){
+        this.idTrabajador = idTrabajador;
+        this.nombre = nombre;
+        this.apellido = apellido;
+    }
+
+    //Metodos getter
+    public String getNombre(){
+        return nombre;
+    } 
+    
+    public int getIdTrabajador(){
+        return idTrabajador;
+    }
+
+    public double pagar(){
+        return 0.0;
+    }
+
+    public String toString(){
+        return "Trabajador { idTrabajador:" + idTrabajador + 
+        "Nombre:" + nombre + 
+        "Apellido: "+ apellido + "}"; 
+    }
+
+
+}
