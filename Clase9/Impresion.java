ebd74f8d1f5aa05fddafc28daab6e6cd2c263fe2
SBarona07 <juan.barona@campusucc.edu.co>
2025-04-10 01:56:33 +0000

    se hiceron los get y los set

--- Clase9/Impresion.java
+++ Clase9/Impresion.java
@@ -2,10 +2,42 @@ package Clase9;
 
 public class Impresion extends Producto {
 
-    //Atributos
+    // Atributos
     private String color;
-    private Foto[] foto;    //Arreglo de fotos
+    private Foto[] foto; // Arreglo de fotos
 
-    //Metodos
-    
+    // Constructor
+    public Impresion(Foto[] foto, String color) {
+        this.color = color;
+        this.foto = foto;
+    }
+
+    // Metodos
+    // get se usa para retornar informacion puntual
+    public String getColor() {
+        return color;
+    }
+
+    public Foto[] getFoto() {
+        return foto;
+    }
+
+    // Set se usa para modificar la info, color.
+    // metodos set y get se realizan solo a los atributos.
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public void setFoto(Foto[] foto) {
+        this.foto = foto;
+    }
+
+    // El toString es el metodo que se usa para mostrar la info de la clase.
+    public String toString() {
+        return "Impresion{" +
+                " color='" + color + '\'' +
+                ", foto=" + java.util.Arrays.toString(foto) +
+                '}';
+    }
 }
--- Clase9/Producto.java
+++ Clase9/Producto.java
@@ -4,8 +4,47 @@ public class Producto {
 
     //Atributos
     private int numero;
+    private String nombre;
+    private String referencia;
+    private double ancho;
+    private double alto;
+
+    //Constructor
+    public Producto(int numero, String nombre, String referencia, double ancho, double alto){
+        this.numero = numero;
+        this.nombre = nombre;
+        this.referencia = referencia;
+        this.ancho = ancho;
+        this.alto = alto;
+    }
 
     //Metodos
+    public int getNumero(){
+        return numero;
+    }
 
-    
+    public String getNombre(){
+        return nombre;
+    }
+
+    public String getReferencia(){
+        return referencia;
+    }
+
+    public double getAncho(){
+        return ancho;
+    }
+
+    public double getAlto(){
+        return alto;
+    }
+
+    public String toString(){
+        return "Producto{" + 
+        "numero=" + numero +
+        ",nombre=" + nombre +
+        ",referencia=" + referencia +
+        ",ancho=" + ancho +
+        ",alto=" + alto + "}";
+    }
 }
